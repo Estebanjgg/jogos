153801fe96ddbfea3e19830ad576d2f6dfc97527
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2022-08-05 00:28:42 -0300

    forca

--- forca.py
+++ forca.py
@@ -1,29 +1,42 @@
 def jogar():
-    print('*****************************************')
-    print('****** Bem-vindo ao jogo da Forca *******')
-    print('*****************************************')
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
-    while (not acertou and not enforcou):
+    while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas [index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
-if(__name__ == '__main__'):
+if(__name__ == "__main__"):
     jogar()
