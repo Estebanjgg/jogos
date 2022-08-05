7611f45565e98dee423bed19bf25f59a6d577c21
Esteban <esteban030991@gmail.com>
2022-08-04 21:57:01 -0300

    cemençando o jogo da forca

---
+++ jogos.py
@@ -0,0 +1,21 @@
+import forca
+import adivinhancao
+
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogo? "))
+
+    if (jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif (jogo == 2):
+        print("Jogando adivinhação")
+        adivinhancao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
